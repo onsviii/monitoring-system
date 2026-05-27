99892ad4ea5c546d0184a8892ec1c64b4222aa3f
Nazar Svitiashchuk <svitiashchuknazar@gmail.com>
2026-05-28 00:17:49 +0300

    init commit

---
+++ src/test/java/ua/bkr/monitor/CompIntelCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.bkr.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompIntelCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
