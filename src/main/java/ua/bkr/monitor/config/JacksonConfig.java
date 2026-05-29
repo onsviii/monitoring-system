8dba969037868719cbb76d319fa7c52a40bf809b
Nazar Svitiashchuk <svitiashchuknazar@gmail.com>
2026-05-29 20:27:29 +0300

    feat: add custom Jackson configuration for ObjectMapper

---
+++ src/main/java/ua/bkr/monitor/config/JacksonConfig.java
@@ -0,0 +1,19 @@
+package ua.bkr.monitor.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.cfg.DateTimeFeature;
+import tools.jackson.databind.json.JsonMapper;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        return JsonMapper.builder()
+                .disable(DateTimeFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .build();
+    }
+}
