9ae50597835a8a8d55cdc08a5d61fe77732af096
Nazar Svitiashchuk <svitiashchuknazar@gmail.com>
2026-05-30 09:07:24 +0300

    feat: add getter for errorType in DataCollectionException to improve error handling

--- backend/comp-intel-core/src/main/java/ua/bkr/monitor/exception/DataCollectionException.java
+++ backend/comp-intel-core/src/main/java/ua/bkr/monitor/exception/DataCollectionException.java
@@ -1,7 +1,9 @@
 package ua.bkr.monitor.exception;
 
+import lombok.Getter;
 import ua.bkr.monitor.model.enums.CollectionErrorType;
 
+@Getter
 public class DataCollectionException extends RuntimeException {
     private final CollectionErrorType errorType;
     private final String placeId;
