735035689ac1c84128a06f0dbeae270e655678da
Nazar Svitiashchuk <svitiashchuknazar@gmail.com>
2026-05-30 13:29:12 +0300

    feat: allow OPTIONS requests and error endpoint access in SecurityConfig

--- backend/comp-intel-core/src/main/java/ua/bkr/monitor/config/SecurityConfig.java
+++ backend/comp-intel-core/src/main/java/ua/bkr/monitor/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package ua.bkr.monitor.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -37,6 +38,8 @@ public class SecurityConfig {
                 )
                 .exceptionHandling(ex -> ex.authenticationEntryPoint(customAuthEntryPoint))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/error").permitAll()
                         .requestMatchers("/api/v1/system/**").hasRole(Role.OPERATOR.name())
                         .requestMatchers("/api/v1/analyses/**").hasRole(Role.BUSINESS.name())
                         .anyRequest().authenticated()
