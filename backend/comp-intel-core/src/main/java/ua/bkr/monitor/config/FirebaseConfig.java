b9b3c85375790546d021f2a0c11558cadd3971ba
Nazar Svitiashchuk <svitiashchuknazar@gmail.com>
2026-05-30 10:59:51 +0300

    feat: update Firebase configuration to use credentials path from application properties

--- backend/comp-intel-core/src/main/java/ua/bkr/monitor/config/FirebaseConfig.java
+++ backend/comp-intel-core/src/main/java/ua/bkr/monitor/config/FirebaseConfig.java
@@ -5,21 +5,28 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
+
+    @Value("${firebase.credentials-path}")
+    private String credentialsPath;
+
     @PostConstruct
     public void init() throws IOException {
         if (FirebaseApp.getApps().isEmpty()) {
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.getApplicationDefault())
-                    .build();
-
-            FirebaseApp.initializeApp(options);
+            try (FileInputStream serviceAccount = new FileInputStream(credentialsPath)) {
+                FirebaseOptions options = FirebaseOptions.builder()
+                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .build();
+                FirebaseApp.initializeApp(options);
+            }
         }
     }
 
